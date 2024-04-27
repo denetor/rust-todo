09392c09d9efa7103d46f5015aa94ad3e0e7119d
Nicola Da Rold <nicola.darold@openview.it>
2024-04-27 07:02:29 +0200

    add: tasks list management

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,30 @@
-mod cli;
-
 use structopt::StructOpt;
+mod cli;
+mod tasks;
+
+use cli::{Action::*, CommandLineArgs};
+use tasks::Task;
+
 
 
 fn main() {
-    println!("{:#?}", cli::CommandLineArgs::from_args());
+    // println!("{:#?}", cli::CommandLineArgs::from_args());
     // cli::CommandLineArgs::from_args();
+
+    // Get the command-line arguments.
+    let CommandLineArgs {
+        action,
+        journal_file,
+    } = CommandLineArgs::from_args();
+
+    // Unpack the journal file.
+    let journal_file = journal_file.expect("Failed to find journal file");
+
+    // Perform the action.
+    match action {
+        Add { task } => tasks::add_task(journal_file, Task::new(task)),
+        List => tasks::list_tasks(journal_file),
+        Done { position } => tasks::complete_task(journal_file, position),
+    }
+    .expect("Failed to perform action")
 }
